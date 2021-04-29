c5f26d8573f1311c5fbbe1d30ae971ac24ab6d72
vladn255 <nesvitvlad@yandex.ru>
2021-04-29 17:12:24 +0400

    5.6. Декоративные элементы главной страницы_корректировка

--- source/js/toggle-menu.js
+++ source/js/toggle-menu.js
@@ -2,20 +2,7 @@ const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.page-header__logo-toggle');
 
 const handleClickToggleMenu = () => {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-
-    navToggle.classList.remove('page-header__logo-toggle--closed');
-    navToggle.classList.add('page-header__logo-toggle--opened');
-
-  } else {
-    navMain.classList.remove('main-nav--opened');
-    navMain.classList.add('main-nav--closed');
-
-    navToggle.classList.remove('page-header__logo-toggle--opened');
-    navToggle.classList.add('page-header__logo-toggle--closed');
-  }
+  navMain.classList.toggle('main-nav--opened');
 }
 
 navMain.classList.remove('main-nav--nojs');
