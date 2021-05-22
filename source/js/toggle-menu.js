02e24c27ff71a5bbe2215f355ef9844223cd79d4
vladn255 <nesvitvlad@yandex.ru>
2021-05-22 19:56:30 +0400

    8.9. Автоматизация сборки личного проекта_корректировка

--- source/js/toggle-menu.js
+++ source/js/toggle-menu.js
@@ -8,3 +8,23 @@ const handleClickToggleMenu = () => {
 navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', handleClickToggleMenu);
+
+ymaps.ready(init);
+function init() {
+  var map = new ymaps.Map("map", {
+    center: [59.938716, 30.323047],
+    zoom: 17,
+    controls: ['zoomControl'],
+    behaviors: ['drag']
+  });
+
+  var placemark = new ymaps.Placemark([59.938635, 30.323118], {},
+    {
+      iconLayout: 'default#image',
+      iconImageHref: './img/icons/map-marker.svg',
+      iconImageSize: [36, 36],
+      iconImageOffset: [-18, -18]
+    });
+
+  map.geoObjects.add(placemark);
+};
